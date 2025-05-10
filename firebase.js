2854319267bb638a8ce35c06bdd7c563e372bbb8
BlooketStevie <cfff82555@gmail.com>
2025-05-09 21:02:30 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,13 @@
-// Replace with your actual config from Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyD-f-UF3nM-R1TSMegY7lW9szHoS7f3WJI",
   authDomain: "armorel-3e2bf.firebaseapp.com",
   projectId: "armorel-3e2bf",
-  storageBucket: "armorel-3e2bf.firebasestorage.app",
+  storageBucket: "armorel-3e2bf.appspot.com",
   messagingSenderId: "899798221811",
   appId: "1:899798221811:web:f90cba6fb4e52f496f0886",
+  measurementId: "G-PXTNXCTHRG"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Firebase references
 const auth = firebase.auth();
 const storage = firebase.storage();
